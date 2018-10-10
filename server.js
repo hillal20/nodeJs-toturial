76006908503e362244581e2d006ed3739203e897
hilal aissani <hilalaissani@gmail.com>
2018-10-09 20:56:26 -0400

    j

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log(" req-url ==>", req.url);
   res.writeHead(200, { "content-type": "text/plain" });
-  res.end("hello  ninja");
+  //////////////////////////////
+  // res.end("hello  ninja");
+  /////////////////////////////// using streams
+  const myReadStream = fs.createReadStream(__dirname + "/sample1.txt", "utf8");
+  myReadStream.pipe(res);
 });
 
 server.listen(3000, "localhost");
