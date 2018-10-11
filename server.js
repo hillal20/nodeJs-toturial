3ca2d6e6c70cce0e8b921e275aad03b510fa26b7
hilal aissani <hilalaissani@gmail.com>
2018-10-10 22:23:14 -0400

    h

--- server.js
+++ server.js
@@ -1,14 +1,41 @@
 const http = require("http");
 const fs = require("fs");
 
+// const server = http.createServer((req, res) => {
+//   console.log(" req-url ==>", req.url);
+//   res.writeHead(200, { "content-type": "text/html" });
+//   res.writeHead(200, { "content-type": "text/plain" });
+//   //////////////////////////////
+//   // res.end("hello  ninja");
+//   /////////////////////////////// using streams
+//   // const myReadStream = fs.createReadStream(__dirname + "/sample1.txt", "utf8");
+//   const myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
+//   myReadStream.pipe(res);
+
+// });
+
 const server = http.createServer((req, res) => {
   console.log(" req-url ==>", req.url);
-  res.writeHead(200, { "content-type": "text/plain" });
+
   //////////////////////////////
-  // res.end("hello  ninja");
-  /////////////////////////////// using streams
-  const myReadStream = fs.createReadStream(__dirname + "/sample1.txt", "utf8");
-  myReadStream.pipe(res);
+
+  ////////////////////////////////////////////////////
+  if (req.url === "/home") {
+    res.writeHead(200, {
+      "content-type": "text/html"
+    });
+    fs.createReadStream(__dirname + "/index.html").pipe(res);
+  } else if (req.url === "/contact") {
+    res.writeHead(200, {
+      "content-type": "text/html"
+    });
+    fs.createReadStream(__dirname + "/contact.html").pipe(res);
+  } else {
+    res.writeHead(200, { "content-type": "application/json" });
+    const obj = { name: "hilal", job: "driver", age: 33 };
+
+    res.end(JSON.stringify(obj));
+  }
 });
 
 server.listen(3000, "localhost");
---
+++ serverExpress.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const server = express();
+server.listen(3000, () => {
+  console.log("=== api is running on port 3000 === ");
+});
