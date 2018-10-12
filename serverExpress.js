3ae1d194b3ddb93e6e9ed84752aed5844a015801
hilal aissani <hilalaissani@gmail.com>
2018-10-11 22:07:06 -0400

    routes

--- serverExpress.js
+++ serverExpress.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const server = express();
+
+server.get("/", (req, res) => {
+  res.send("api is running ");
+});
+
+server.get("/contact", (req, res) => {
+  res.send("contact is here  ");
+});
+
+server.get("/contact/:id", (req, res) => {
+  res.send(`${req.params.id} is here and ${req.query.name}  `);
+});
+
 server.listen(3000, () => {
   console.log("=== api is running on port 3000 === ");
 });
