6a51c4332dedf3e2d2eed5fd5d18a76cb818b71f
hilal aissani <hilalaissani@gmail.com>
2018-10-13 11:04:46 -0400

    k

--- advencedNodeJs/async.js
+++ advencedNodeJs/async.js
@@ -24,19 +24,21 @@ const asyncFunction = async () => {
   /// async function always return promise
 
   const promise = new Promise((resolve, reject) => {
-    return resolve("person3 ticket");
+    setTimeout(() => {
+      resolve("person3 ticket");
+    }, 3000);
   });
 
   let person3Ticket = await promise;
-
+  console.log(person3Ticket);
   console.log("person6 ticket");
   console.log("person7 ticket");
 
-  return person3Ticket;
+  return "person9 ticket";
 };
 asyncFunction().then(msg => {
+  console.log("person8 ticket");
   console.log(msg);
 });
-
 console.log("person4 ticket");
 console.log("person5 ticket");
