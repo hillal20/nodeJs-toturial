ae9e2a27e501d0e3903197df48d04dad7e9dc2ef
hilal aissani <hilalaissani@gmail.com>
2018-10-16 23:15:11 -0400

    jj

---
+++ doublexStream.js
@@ -0,0 +1,16 @@
+const { Duplex, PassThrough } = require("stream");
+const { createReadStream, createWriteStream } = require("fs");
+
+const readStream = createReadStream("./vedio.mp4");
+const writeStream = createWriteStream("./cpy.mp4");
+
+const report = new PassThrough(); // doublex
+
+//////////////////////
+let total = 0;
+report.on("data", chunk => {
+  total += chunk.length;
+  console.log("bytes", total);
+});
+
+readStream.pipe(report).pipe(writeStream);
