7c5a7d9c2961f1d43c8584b6cab0c5ffc5d0d5df
hilal aissani <hilalaissani@gmail.com>
2018-10-18 18:29:39 -0400

    browser

--- streamToBrowser.js
+++ streamToBrowser.js
@@ -6,9 +6,25 @@ const file = "./vedio.mp4";
 
 const server = createServer(async (req, res) => {
   const { size } = await info(file);
-  console.log(size);
-  res.writeHead(200, { "Content-Length": size, "Content-type": "video/mp4" });
-  createReadStream(file).pipe(res);
+  //console.log(size);
+  //////////// range
+  console.log(req.headers);
+  const range = req.headers.range;
+  if (range) {
+    let [start, end] = range.replace(/bytes=/g, "").split("-");
+    start = parseInt(start, 10);
+    end = end ? parseInt(end, 10) : size - 1;
+    res.writeHead(206, {
+      "Content-range": `bytes ${start}-${end}/${size}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": end - start + 1,
+      "Content-type": "video/mp4"
+    });
+    createReadStream(file, { start, end }).pipe(res);
+  } else {
+    res.writeHead(200, { "Content-Length": size, "Content-type": "video/mp4" });
+    createReadStream(file).pipe(res);
+  }
 });
 server.listen(3000, () => {
   console.log("== server is running on 3000");
