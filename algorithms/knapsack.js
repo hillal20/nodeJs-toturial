efd378e114d48ff4766d4be6f197746af0b7a67d
hilal aissani <hilalaissani@gmail.com>
2018-10-29 19:33:30 -0400

    knapsack2

--- algorithms/knapsack.js
+++ algorithms/knapsack.js
@@ -4,4 +4,16 @@ const knapsack = () => {
   let maxW = 8;
   let n = profit.length;
   let ks = [[], [], [], [], []];
+
+  for (let i = 0; i < n; i++) {
+    for (let w = 0; w <= maxW; w++) {
+      if (i === 0 || w === 0) {
+        k[i][w] = 0;
+      } else if (wight[i] <= w) {
+        k[i][w] = Math.max(profit[i] + k[i - 1][w - wight[i]], k[i - 1][w]);
+      } else {
+        k[i][w] = k[i - 1][w];
+      }
+    }
+  }
 };
