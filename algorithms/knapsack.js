aa9eac35ded56e36460127ad262fef6746e6520a
hilal aissani <hilalaissani@gmail.com>
2018-10-29 20:08:49 -0400

    knapsack3

--- algorithms/knapsack.js
+++ algorithms/knapsack.js
@@ -1,19 +1,65 @@
-const knapsack = () => {
-  let profit = [0, 1, 2, 5, 6];
-  let wight = [0, 2, 3, 4, 5];
-  let maxW = 8;
-  let n = profit.length;
-  let ks = [[], [], [], [], []];
+const fs = require("fs");
+const argv = process.argv.slice(2);
 
-  for (let i = 0; i < n; i++) {
-    for (let w = 0; w <= maxW; w++) {
-      if (i === 0 || w === 0) {
-        k[i][w] = 0;
-      } else if (wight[i] <= w) {
-        k[i][w] = Math.max(profit[i] + k[i - 1][w - wight[i]], k[i - 1][w]);
-      } else {
-        k[i][w] = k[i - 1][w];
-      }
+console.log("argv ==>", argv);
+
+if (argv.length != 2) {
+  console.error("usage: filename capacity");
+  process.exit(1);
+}
+
+const filename = argv[0];
+const capacity = parseInt(argv[1]);
+
+// Read the file
+const fileData = fs.readFileSync(filename, "utf8");
+// Split the fileData on each new line
+const lines = fileData.trim().split(/[\r\n]+/g);
+
+// Process the lines
+const items = [];
+
+for (let l of lines) {
+  //console.log(lines);
+  const [index, size, value] = l.split(" ").map(n => parseInt(n));
+
+  items.push({
+    index: index,
+    size: size,
+    value: value
+  });
+}
+
+//////////////////////////////////////// iterative ///////
+
+greedyAlo = (items, capacity) => {
+  const result = {
+    size: 0,
+    value: 0,
+    chosen: []
+  };
+  // console.log(items);
+
+  // items = items.filter(item => item.size < capacity);
+  items.sort((item1, item2) => {
+    const x = item1.value / item1.size;
+    const y = item2.value / item2.size;
+
+    return y - x;
+  });
+  console.log(items);
+
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].size <= capacity) {
+      // if it is, add it to our final result
+      result.size += items[i].size;
+      result.value += items[i].value;
+      result.chosen.push(items[i].index);
+      // don't forget to decrement our total capacity
+      capacity -= items[i].size;
     }
   }
+  console.log(result);
+  return result;
 };
+greedyAlo(items, capacity);
---
+++ algorithms/knapsack1.js
@@ -0,0 +1,21 @@
+const knapsack = () => {
+  let profit = [0, 1, 2, 5, 6];
+  let wight = [0, 2, 3, 4, 5];
+  let maxW = 8;
+  let n = profit.length;
+  let k = [[], [], [], [], []];
+
+  for (let i = 0; i < n; i++) {
+    for (let w = 0; w <= maxW; w++) {
+      if (i === 0 || w === 0) {
+        k[i][w] = 0;
+      } else if (wight[i] <= w) {
+        k[i][w] = Math.max(profit[i] + k[i - 1][w - wight[i]], k[i - 1][w]);
+      } else {
+        k[i][w] = k[i - 1][w];
+      }
+    }
+  }
+  return k;
+};
+console.log(knapsack());
