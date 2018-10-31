cbf27a43ee521a0d64fd473393ef0455ca52f535
hilal aissani <hilalaissani@gmail.com>
2018-10-31 15:36:27 -0400

    bt

---
+++ algorithms/heap.js
@@ -0,0 +1,49 @@
+class Heap {
+  constructor() {
+    this.storage = [];
+  }
+  insert(x) {
+    if (this.storage.length > 0) {
+      if (x >= this.storage[0]) {
+        this.storage.unshift(x);
+      }
+      if (x < this.storage[0]) {
+        this.storage.push(x);
+        this.bubbleUp(this.storage.length - 1);
+      }
+    } else {
+      this.storage.push(x);
+    }
+  }
+  delete() {
+    const res;
+    if(this.storage.length === 0) return null;
+    if(this.storage.length > 0){
+       res = this.storage.shift();
+       this.siftDown(this.storage[0])
+    }
+    return res;
+  }
+
+  bubbleUp(index) {
+    const parentIndex = Math.floor(index - 1 / 2);
+    if (this.storage[index] > this.storage[parentIndex]) {
+      [this.storage[parentIndex], this.storage[index]] = [
+        this.storage[index],
+        this.storage[parentIndex]
+      ];
+      this.bubbleUp(parentIndex);
+    }
+  }
+  siftDown(index) {
+  const leftChildIndex = 2*index +1;
+  const rightChildIndex = 2*index + 2;
+  const maxIndex = this.storage[leftChildIndex] > this.storage[rightChildIndex]? leftChildIndex : rightChildIndex;
+  if( this.storage[index] <  this.storage[maxIndex]){
+    [this.storage[maxIndex], this.storage[index]] = [this.storage[index],this.storage[maxIndex] ]
+    this.siftDown(maxIndex)
+  }
+   
+
+  }
+}
