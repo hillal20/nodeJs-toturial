0d5c95b4940b2dfb251a322712fdfdc18b980256
hilal aissani <hilalaissani@gmail.com>
2018-10-31 16:43:30 -0400

    bt

--- algorithms/stairs.js
+++ algorithms/stairs.js
@@ -1,10 +1,16 @@
-const numOfStairs = 5;
+const numOfStairs = 3;
+
 const fn = n => {
-  if (n <= 0) {
+  let result = 0;
+
+  if (n === 0) {
     return 1;
   }
+  if (n - 1 >= 0 || n - 2 >= 0 || n - 3 >= 0) {
+    result += fn(n - 1) + fn(n - 2) + fn(n - 3);
+  }
 
-  return 1 + fn(n - 1) + fn(n - 2) + fn(n - 3);
+  return result;
 };
 
 console.log(fn(numOfStairs));
