e58cd350a99d45e4295ef46a5fa42c87a7b5cdf3
hilal aissani <hilalaissani@gmail.com>
2018-11-03 21:00:51 -0400

    still" ; "

--- algorithms/str.js
+++ algorithms/str.js
@@ -8,32 +8,35 @@ let current = "";
 let count = 0;
 let loopIndex = 0;
 let arrLoopIndex = [];
-for (let i = 0; i < str.length; i++) {
-  if (!isNaN(parseInt(str[i]))) {
+let str2 = str;
+// let passed = false;
+
+for (let i = 0; i < str2.length; i++) {
+  console.log(loopIndex);
+  if (!isNaN(parseInt(str2[i]))) {
     cIndex = i;
 
     while (true) {
-      cChar = str[cIndex];
+      cChar = str2[cIndex];
       if (!isNaN(parseInt(cChar))) {
-        cChar = str[cIndex++];
+        cChar = str2[cIndex++];
         count++;
         current += cChar;
         loopIndex += cIndex + count;
       } else {
         newArr.push(current);
+        // str2 = str2.slice(loopIndex);
 
         count = 0;
         current = "";
+        cChar = "";
         cIndex = 0;
-
         break;
       }
     } // while loop
   }
+  // loopIndex;
 
-  // loopIndex = 0;
-  i++;
   // if
 } // for loop
 console.log(newArr);
-console.log(arrLoopIndex);
