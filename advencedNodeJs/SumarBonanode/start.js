da35ead0b7f5f27a75f2414c0a7258693354e88d
hilal aissani <hilalaissani@gmail.com>
2018-11-12 18:46:06 -0500

    buffer

--- advencedNodeJs/SumarBonanode/start.js
+++ advencedNodeJs/SumarBonanode/start.js
@@ -27,7 +27,7 @@ const { config } = require("./util.js");
 // replGlobal.context.lodash = require("lodash");
 // console.log(replGlobal);
 
-// =====> global object
+// =====> global object = (global.process , global.buffer)
 
 // console.log(global.process);
 // console.log(global.Buffer);
@@ -37,7 +37,49 @@ const { config } = require("./util.js");
 
 // ===> process
 
-process.on("exit", () => {
-  console.log("exiting.....");
+// process.on("exit", () => {
+//   console.log("exiting.....");
+// });
+// process.on("uncaughtException", err => {
+//   console.log("err ==>", err.message);
+//   process.exit(1);
+// });
+
+// process.stdin.resume(); // keep the event loop busy
+// console.log("after std.resume()");
+// console.dog(); //// ==>  code err
+
+////===>  buffer  = array allocated outside the v8 heap
+
+// const arr1 = Buffer.alloc(8);
+// console.log("arr==>1", arr1);
+
+// const arr2 = Buffer.allocUnsafe(8);
+// console.log("arr2==> ", arr2);
+
+// const string = "steve";
+// const buffer = Buffer.from("steve");
+// console.log("string ===> ", string, string.length);
+// console.log("buffer==>", buffer, buffer.length);
+
+// example
+const fs = require("fs");
+
+const conversionMap = {
+  "88": "65",
+  "89": "66",
+  "90": "67"
+};
+
+fs.readFile(__filename, (err, data) => {
+  console.log(data);
+  let tag = data.slice(-9, -1);
+  console.log(tag);
+
+  for (let i = 0; i < tag.length; i++) {
+    tag[i] = conversionMap[tag[i]];
+    console.log(data.toString());
+    console.log(tag.toString());
+  }
 });
-process.on("uncaughtException", err => {});
+// tag : xyz
