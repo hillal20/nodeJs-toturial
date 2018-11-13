eadf42b82909d92127aa0f098ba1d7335ee4ded6
hilal aissani <hilalaissani@gmail.com>
2018-11-13 18:30:48 -0500

    json

---
+++ advencedNodeJs/SumarBonanode/json/json.js
@@ -0,0 +1,5 @@
+//  require('something')
+
+// 1. try something.js
+// 2. try something.json
+// 3. try  something.node
---
+++ advencedNodeJs/SumarBonanode/module.js
@@ -0,0 +1,5 @@
+//console.log(module);
+
+//require.resolve("hilal");
+require("hilal");
+console.log("===> module");
