1625835f6b56ad2125e9209ff53b8ba1ac9b0fad
hilal aissani <hilalaissani@gmail.com>
2018-11-16 19:01:37 -0500

    json

--- advencedNodeJs/SumarBonanode/json/json.js
+++ advencedNodeJs/SumarBonanode/json/json.js
@@ -3,3 +3,10 @@
 // 1. try something.js
 // 2. try something.json
 // 3. try  something.node
+
+// ====>
+
+// const data = require("config");
+// console.log(data);
+//console.log(arguments);
+console.log("*".repeat(10));
