8c0c101aacf1765ce767a8b708c0eaa128b91c6c
hilal aissani <hilalaissani@gmail.com>
2018-11-20 19:10:42 -0500

    sockets

--- advencedNodeJs/SumarBonanode/eventLoopModule/networking/net.js
+++ advencedNodeJs/SumarBonanode/eventLoopModule/networking/net.js
@@ -1,11 +1,24 @@
 const net = require("net");
 const server = net.createServer();
+let counter = 0;
+let connectedSockets = {};
+
 server.on("connection", socket => {
-  console.log("==  dear server i am the client  ");
-  socket.write(" == server is saying welcome to client === \n");
+  /////
+  socket.id = counter++;
+  connectedSockets[socket.id] = socket;
+  /////
+  console.log("==   client connecting to server  ");
+  /////////
+
+  socket.write(" == server responding to client === \n");
   socket.on("data", data => {
-    console.log("data is ===>:", data);
-    socket.write(data);
+    console.log(`data from ${socket.id} is ===>:`, data);
+    //////////////
+    Object.entries(connectedSockets).forEach(([key, connectedSocket]) => {
+      connectedSocket.write(`==> ${socket.id}: `);
+      connectedSocket.write(data);
+    });
   });
   socket.setEncoding("utf8");
 });
