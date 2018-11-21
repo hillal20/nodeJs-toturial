77ba41ef9eaf5d07e21a64d01dfab57c44634d51
hilal aissani <hilalaissani@gmail.com>
2018-11-20 20:15:44 -0500

    sockets

--- advencedNodeJs/SumarBonanode/eventLoopModule/networking/net.js
+++ advencedNodeJs/SumarBonanode/eventLoopModule/networking/net.js
@@ -6,17 +6,35 @@ let connectedSockets = {};
 server.on("connection", socket => {
   /////
   socket.id = counter++;
-  connectedSockets[socket.id] = socket;
+  /////
+  const timestamp = () => {
+    const now = new Date();
+    return `${now.getHours()}:${now.getMinutes()}`;
+  };
+
   /////
   console.log("==   client connecting to server  ");
   /////////
-
   socket.write(" == server responding to client === \n");
+  socket.write(" *** please type your name : \n");
+  //////
+
   socket.on("data", data => {
+    if (!connectedSockets[socket.id]) {
+      socket.name = data.toString().trim();
+      socket.write(`welcome ${socket.name}  \n`);
+      connectedSockets[socket.id] = socket;
+      return;
+    }
+
     console.log(`data from ${socket.id} is ===>:`, data);
     //////////////
     Object.entries(connectedSockets).forEach(([key, connectedSocket]) => {
-      connectedSocket.write(`==> ${socket.id}: `);
+      if (socket.id == key) {
+        return;
+      }
+      console.log(key);
+      connectedSocket.write(`==> ${socket.name} ${timestamp()}: `);
       connectedSocket.write(data);
     });
   });
