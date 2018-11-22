f573f22e7fad237211e6393e1c7ce5aa2e0007bf
hilal aissani <hilalaissani@gmail.com>
2018-11-21 19:38:40 -0500

    udps

--- advencedNodeJs/SumarBonanode/eventLoopModule/dns/udp.js
+++ advencedNodeJs/SumarBonanode/eventLoopModule/dns/udp.js
@@ -1,5 +1,7 @@
 const dgram = require("dgram");
 const server = dgram.createSocket("udp4");
+const port = 3333;
+const host = "localhost";
 
 /////////////////// server
 
@@ -8,18 +10,25 @@ server.on("message", (msg, remoteInfo) => {
   console.log("rinfo ===>", remoteInfo);
 });
 
-const port = 3333;
-const host = "localhost";
-server.bind(port, host);
-
 server.on("listening", () => {
   console.log("===> udp  server listening ===");
 });
 
+server.bind(port, host);
+
 ////////// client
 const client = dgram.createSocket("udp4");
+
 client.send("pluralsight rocks", port, host, err => {
   if (err) throw err;
   console.log("==> udp message sent === ");
+  client.close();
+});
+
+/////  we can send buffer
+const bufferMsg = Buffer.from("hilal aissani");
+client.send(bufferMsg, 0, bufferMsg.length, port, host, err => {
+  if (err) throw err;
+  console.log("==> udp bufferMessage sent === ");
   client.close();
 });
