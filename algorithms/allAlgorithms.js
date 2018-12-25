4b77bcd0e32c5ecb6cda754390d425062a58d33b
hilal aissani <hilalaissani@gmail.com>
2018-12-24 20:25:19 -0500

    all algo

--- algorithms/allAlgorithms.js
+++ algorithms/allAlgorithms.js
@@ -537,3 +537,58 @@ function Merge(a, b, n, m) {
   return c;
 }
 console.log(Merge(a, b, a.length, b.length));
+class BinaryTreeNode {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insertLeft(value) {
+    this.left = new BinaryTreeNode(value);
+    return this.left;
+  }
+
+  insertRight(value) {
+    this.right = new BinaryTreeNode(value);
+    return this.right;
+  }
+}
+
+/* 
+  A recursive solution
+  How would you solve this iteratively?
+ */
+const checkBalanced = rootNode => {
+  // An empty tree is balanced by default
+  if (!rootNode) return true;
+  // recursive helper function to check the min depth of the tree
+  const minDepth = node => {
+    if (!node) return 0;
+    return 1 + Math.min(minDepth(node.left), minDepth(node.right));
+  };
+  // recursive helper function to check the max depth of the tree
+  const maxDepth = node => {
+    if (!node) return 0;
+    return 1 + Math.max(maxDepth(node.left), maxDepth(node.right));
+  };
+
+  return maxDepth(rootNode) - minDepth(rootNode) === 0;
+};
+
+/* Some console.log tests */
+
+const root = new BinaryTreeNode(5);
+console.log("root ===>5", root);
+
+console.log(checkBalanced(root)); // should print true
+
+root.insertLeft(10);
+console.log("root ===>10", root);
+
+console.log(checkBalanced(root)); // should print false
+
+root.insertRight(11);
+console.log("root ===>", root);
+
+console.log(checkBalanced(root)); // should print true;
