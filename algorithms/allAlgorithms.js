81f422fecb8feed0fd9b96e34a92b18b503867ac
hilal aissani <hilalaissani@gmail.com>
2018-12-27 18:59:41 -0500

    all algo2

--- algorithms/allAlgorithms.js
+++ algorithms/allAlgorithms.js
@@ -1,29 +1,3 @@
-const mergeSort = arr => {
-  if (arr.length < 2) {
-    return arr;
-  }
-  const middle = Math.floor(arr.length / 2);
-
-  const right = arr.slice(0, middle);
-  const left = arr.slice(middle, arr.length);
-  return merge(mergeSort(left), mergeSort(right));
-};
-
-const merge = (left, right) => {
-  const result = [];
-
-  while (left.length && right.length) {
-    if (left[0] <= right[0]) {
-      result.push(left.shift());
-    } else {
-      result.push(right.shift());
-    }
-  }
-
-  return result.concat(left, right);
-};
-mergeSort([1, 2, 3, 9, 3, 2, 9, 4, 8, 5]);
-
 ////////////////////////////////// quicksort
 
 const quicksort = arr => {
